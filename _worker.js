5b2373ef3db0e712ae1c80f84ea28139d5b5a29f
mkhrrs89 <47932065+mkhrrs89@users.noreply.github.com>
2026-09-09 17:40:33 -0400

    Restore unrelated audit script versions

--- _worker.js
+++ _worker.js
@@ -304,9 +304,9 @@ export default {
           + '<script src="/habit_ledger_matchup_impact_dateiso.js?v=20260801-1" data-taskpoints-habit-matchup-dateiso="true"></script>'
           + '<script src="/habit_ledger_matchup_impact_legacy_scores.js?v=20260801-1" data-taskpoints-habit-matchup-legacy-scores="true"></script>'
           + '<script src="/habit_ledger_matchup_restore_transform.js?v=20260801-1" data-taskpoints-habit-matchup-restore-transform="true"></script>'
-          + '<script src="/habit_ledger_matchup_impact_attestation.js?v=20260801-1" data-taskpoints-habit-matchup-attestation="true"></script>'
-          + '<script src="/habit_ledger_matchup_restore_ui.js?v=20260731-1" data-taskpoints-habit-matchup-restore-ui="true"></script>'
-          + '<script src="/habit_ledger_matchup_restore_apply.js?v=20260731-1" data-taskpoints-habit-matchup-restore-apply="true"></script>'
+          + '<script src="/habit_ledger_matchup_impact_attestation.js?v=20260801-1" data-taskpoints-habit-matchup-impact-attestation="true"></script>'
+          + '<script src="/habit_ledger_matchup_restore_ui.js?v=20260801-1" data-taskpoints-habit-matchup-restore-ui="true"></script>'
+          + '<script src="/habit_ledger_matchup_restore_apply.js?v=20260801-1" data-taskpoints-habit-matchup-restore-apply="true"></script>'
           + '<script src="/habit_ledger_matchup_impact_stale_guard.js?v=20260803-3" data-taskpoints-habit-matchup-stale-guard="true"></script>'
           + '<script src="/score_alias_audit_bootstrap.js?v=20260731-2" data-taskpoints-score-alias-audit-bootstrap="true"></script>'
         : '';
