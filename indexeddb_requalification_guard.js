2e5aea707a2311f0d4baca1af532d33139b2c009
mkhrrs89 <47932065+mkhrrs89@users.noreply.github.com>
2026-09-13 12:11:52 -0400

    Load rankings tournament trophies

--- indexeddb_requalification_guard.js
+++ indexeddb_requalification_guard.js
@@ -226,6 +226,19 @@
   document.head.appendChild(script);
 })(typeof window !== 'undefined' ? window : globalThis);
 
+;(function loadTaskPointsRankingsTournamentTrophies(global) {
+  'use strict';
+  const document = global.document;
+  const path = String(global.location?.pathname || '');
+  if (!/(^|\/)rankings(?:\.html)?$/i.test(path)) return;
+  if (!document?.head || document.querySelector?.('script[data-taskpoints-rankings-tournament-trophies]')) return;
+  const script = document.createElement('script');
+  script.src = 'rankings_tournament_trophies.js';
+  script.defer = true;
+  script.dataset.taskpointsRankingsTournamentTrophies = 'true';
+  document.head.appendChild(script);
+})(typeof window !== 'undefined' ? window : globalThis);
+
 ;(function loadTaskPointsScoreAliasConsistency(global) {
   'use strict';
   const document = global.document;
