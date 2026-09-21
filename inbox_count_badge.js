f8f370a0f57d76cf991fd47c33fbbd230230edaf
mkhrrs89 <47932065+mkhrrs89@users.noreply.github.com>
2026-09-21 10:29:46 -0400

    Use Notes authority in secondary exports

--- inbox_count_badge.js
+++ inbox_count_badge.js
@@ -440,8 +440,12 @@
         try { notesText = global.syncNotesStorageLocations('responsive-export-sync'); } catch (_) {}
       } else {
         try {
-          const cached = global.localStorage?.getItem?.('taskpoints_notes_v1') || '';
-          if (cached.length >= notesText.length) notesText = cached;
+          const cachedRaw = global.localStorage?.getItem?.('taskpoints_notes_v1');
+          const cached = cachedRaw || '';
+          const cacheDirty = global.localStorage?.getItem?.('taskpoints_notes_dirty_v1') === '1';
+          const cacheAuthoritative = global.localStorage?.getItem?.('taskpoints_notes_authoritative_v1') === '1';
+          if (cachedRaw !== null && cachedRaw !== undefined && (cacheDirty || cacheAuthoritative)) notesText = cached;
+          else if (cached.length >= notesText.length) notesText = cached;
         } catch (_) {}
       }
       normalized.notes = notesText;
